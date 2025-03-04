6fa0cb950dac00bee4c91fab4676e908b360478d
Tanim <abdullahtanim27@gmail.com>
2025-03-04 09:34:49 +0600

    organized the data object in a json folder

---
+++ src/JSON/Data.jsx
@@ -0,0 +1,103 @@
+export const data = {
+	destinations: [
+		{ name: 'Bali, Indonesia', image: '/images/destinations/bali.jpeg' },
+		{
+			name: 'Santorini, Greece',
+			image: '/images/destinations/santorini.jpg',
+		},
+		{ name: 'Kyoto, Japan', image: '/images/destinations/kyoto.jpg' },
+		{ name: 'Paris, France', image: '/images/destinations/paris.jpg' },
+		{ name: 'Maldives', image: '/images/destinations/maldives.jpg' },
+	],
+	features: {
+		'Best Prices': 'We offer unbeatable travel deals.',
+		'Expert Guides': 'Our guides ensure an amazing experience.',
+		'24/7 Support': "We're here for you anytime, anywhere.",
+	},
+	testimonials: [
+		{
+			name: 'Sujon Howlader',
+			review:
+				'Hijrat Air Travels made my trip unforgettable! The service was amazing, and the destinations were breathtaking!',
+			image: '/images/team/person.jpg',
+		},
+		{
+			name: 'Iqbal Karim',
+			review:
+				'Amazing service and smooth booking! Highly recommend Hijrat Air Travels for your next adventure.',
+			image: '/images/team/person.jpg',
+		},
+	],
+	team: [
+		{
+			name: 'Md Asraful Hoque',
+			role: 'Chairman',
+			image: '/images/team/Chairman.jpg',
+			linkedin: 'https://www.linkedin.com/in/md-asraful-hoque-277858238/',
+			whatsapp: '',
+			facebook: 'https://www.facebook.com/asraful1691',
+		},
+		{
+			name: 'Jahid Mahbub',
+			role: 'Vice President',
+			image: '/images/team/VicePresident.jpg',
+			linkedin: 'https://www.linkedin.com/in/arabby1/',
+			whatsapp: '',
+			facebook: 'https://www.facebook.com/arabby1',
+		},
+		{
+			name: 'Ziaul Haque',
+			role: 'CEO',
+			image: '/images/team/CEO.jpg',
+			linkedin: '',
+			whatsapp: '',
+			facebook: 'https://www.facebook.com/ziaul.haque.960566',
+		},
+		{
+			name: 'Jahangir Alam',
+			role: 'Manager',
+			image: '/images/team/Manager.jpg',
+			linkedin: '',
+			whatsapp: '',
+			facebook: 'https://www.facebook.com/profile.php?id=100010862076691',
+		},
+		{
+			name: 'Nanto Chandro',
+			role: 'Branch Manager, Muradnagar, Cumilla',
+			image: '/images/team/BranchManager.jpg',
+			linkedin: '',
+			whatsapp: '',
+			facebook: 'https://www.facebook.com/nanto.chandra',
+		},
+		{
+			name: 'Habibul Hoque',
+			role: 'Branch Executive, Bakhrabad, Cumilla',
+			image: '/images/team/BranchExecutive.jpg',
+			linkedin: '',
+			whatsapp: '',
+			facebook: 'https://www.facebook.com/mdhabibulhaq.tushar',
+		},
+		{
+			name: 'Abdul Kader',
+			role: 'Public Relation Officer',
+			image: '/images/team/PRO.jpg',
+			linkedin: '',
+			whatsapp: '',
+			facebook: 'https://www.facebook.com/abdulkader.khan.9469',
+		},
+		{
+			name: 'Zakariya Shanto',
+			role: 'Reservation Officer & Executive',
+			image: '/images/team/ROE.jpg',
+			linkedin: '',
+			whatsapp: '',
+			facebook: 'https://www.facebook.com/fatuuu.shanto',
+		},
+	],
+	chairman: {
+		name: 'Md Asraful Hoque',
+		image: '/images/team/Chairman.jpg',
+		message:
+			'At Hijrat Air Travels, we are committed to providing exceptional service and ensuring that every journey is a memorable one. Our team works tirelessly to make your travel dreams a reality. Thank you for trusting us with your travel needs.',
+	},
+}
